c8ff730f7d4867c91feb71992d05ca8230d83626
Yonatan Karni <ykarni@outbrain.com>
2019-04-04 08:20:50 +0300

    added TournamentRunnerFirstState which extends MyTournamentRunner with a different player allocation - 4 copies of OurAgent vs. 3 copies of D-Brane. this can be launched by running the test MyTournamentRunnerTest.testRunTournamentFourVsThree

---
+++ bandana/src/main/java/ddejonge/bandana/tournament/TournamentRunnerFirstStage.java
@@ -0,0 +1,37 @@
+package ddejonge.bandana.tournament;
+
+import ddejonge.bandana.tools.ProcessRunner;
+
+import java.io.File;
+
+public class TournamentRunnerFirstStage extends MyTournamentRunner {
+  Process createPlayerProcess(int finalYear, String tournamentLogFolderPath, int gameNumber, int playerIdx) {
+    String name;
+    String[] command;
+
+    //make sure that each player has a different name.
+    if (playerIdx < 4) {
+      name = "OurAgent" + playerIdx;
+      command = ourAgentCommand;
+    } else {
+      name = "D-Brane " + playerIdx;
+      command = dbrane_1_1_Command;
+    }
+
+    //set the log folder for this agent to be a subfolder of the tournament log folder.
+    command[4] = tournamentLogFolderPath + File.separator + name + File.separator + "Game " + gameNumber + File.separator;
+
+    //set the name of the agent.
+    command[6] = name;
+
+    //set the year after which the agent will propose a draw to the other agents.
+    command[8] = "" + finalYear;
+
+    //start the process
+    String processName = name;
+    // We give  a name to the process so that we can see in the console where its output comes from.
+    // This name does not have to be the same as the name given to the agent, but it would be confusing
+    // to do otherwise.
+    return ProcessRunner.exec(command, processName);
+  }
+}
--- bandana/src/test/java/ddejonge/bandana/MyTournamentRunnerTest.java
+++ bandana/src/test/java/ddejonge/bandana/MyTournamentRunnerTest.java
@@ -1,14 +1,21 @@
 package ddejonge.bandana;
 
 import ddejonge.bandana.tournament.MyTournamentRunner;
+import ddejonge.bandana.tournament.TournamentRunnerFirstStage;
 import org.junit.Test;
 
 import java.io.IOException;
 
 public class MyTournamentRunnerTest {
   @Test
-  public void testRunTournament() throws IOException {
+  public void testRunTournamentOneOfSeven() throws IOException {
     MyTournamentRunner mtr = new MyTournamentRunner();
+    mtr.runTournament();
+  }
+
+  @Test
+  public void testRunTournamentFourVsThree() throws IOException {
+    TournamentRunnerFirstStage mtr = new TournamentRunnerFirstStage();
     mtr.runTournament();
   }
 }
